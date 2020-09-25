b1f743aa99312852080907168a083e98b50816ff
System Administrator <jon.s.solari@gmail.com>
2020-09-25 15:51:21 -0400

    initial commit placeholder text

---
+++ src/components/Slide.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="box">
+  <p class="text">{{this.title}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Slide',
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.box{
+  display: flex;
+  background-color: red;
+  width: 200px;
+  height: 200px;
+  margin: 5px;
+}
+
+.text{
+  color: white;
+  font-size: 1.9rem;
+  font-family: helvetica;
+  font-weight: bold;
+  margin: auto;
+}
+
+</style>
