f5f5581ae8a9b94cd0fb4534f6a093c138836b06
System Administrator <jon.s.solari@gmail.com>
2020-10-05 19:41:17 -0400

    made default words dynamic

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
 
     <div class="slides">
-    <Slide class="left" title="VIDEOGAME" /> <Slide class="middle" title="SANDWICH"/> <Slide class="right" title="DIAMONDS" />
+    <Slide class="left" :title="this.left" /> <Slide class="middle" :title="this.center"/> <Slide class="right" :title="this.right" />
     </div>
 
     <div class="inputs">
-      <input type="text" class="boxes" placeholder="LEFT">
+      <input type="text" class="boxes" placeholder="LEFT" @change="leftHandler">
       <input type="text" class="boxes" placeholder="CENTER">
       <input type="text" class="boxes" placeholder="RIGHT">
     </div>
@@ -18,21 +18,21 @@
 <script>
 import Slide from './components/Slide.vue'
 
-// function data(e){
-//   return e
-// }
+
 
 export default {
   name: 'App',
   components: {
     Slide
+  }, 
+  data(){
+    return{
+      left: 'VIDEOGAME',
+      center: 'SANDWICH',
+      right: 'DIAMONDS'
+    }
   }
 }
-
-// function changeHandler(e){
-
-// }
-
 </script>
 
 <style>
